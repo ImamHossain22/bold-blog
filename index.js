5dfb39eb39df4d792258a399c689567bb056d435
Chowdhury Imam Hossain <121708728+ImamHossain22@users.noreply.github.com>
2023-11-16 01:59:00 +0600

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 
 // middleware
 
-app.use(cors({origin:['https://bold-stylish-blog.web.app/','https://bold-stylish-blog.firebaseapp.com/']}));
+app.use(cors());
 app.use(express.json());
 
 
